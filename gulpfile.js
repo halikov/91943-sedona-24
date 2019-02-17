bbe47d6dc487c86f422986388b73a8f4bc50ab6c
Mikail Halikov <mikail@halikov.com>
2019-02-17 19:39:03 +0300

    стилизует placeholder в формах поиска и фильтре

--- gulpfile.js
+++ gulpfile.js
@@ -8,22 +8,38 @@ const sourcemaps = require('gulp-sourcemaps');
 const debug = require('gulp-debug');
 const del = require('del');
 
+gulp.task('clean', function() {
+  // удаляет содержимое подпапки /out, чтоб перезаписать новый
+  return del('css/out', 'js/out');
+});
+
 gulp.task('styles', function() {
    return gulp.src('css/style.css')
-    .pipe(sourcemaps.init())
+   // sourcemaps в продакшене (в готовом проекте) вроде не нужен
+   //  только во время разработки
+    // инициализирует sourcemaps
+    // .pipe(sourcemaps.init())
+    // автопрефиксер проставляет префиксы движков браузеров
     .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
     }))
-    // .pipe(csso())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('css'));
+    // сжимает css код
+    .pipe(csso())
+    // записывает sourcemaps '.' в отдельный файл, () в файл стилей
+    // .pipe(sourcemaps.write('.'))
+    // создает подпапку и записывает выходной файл в нее
+    .pipe(gulp.dest('css/out'));
 });
 
 gulp.task('compress', function() {
   return gulp.src('js/main.js')
+    // сжимает js  код
     .pipe(uglify())
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('js/out'));
 });
 
-gulp.task('build', gulp.series('styles', 'compress'));
+gulp.task('build',
+  gulp.series('clean',
+  gulp.parallel('styles', 'compress'))
+);
